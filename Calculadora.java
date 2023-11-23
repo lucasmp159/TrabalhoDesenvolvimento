5573c74ca9d6ac91b6555fee206a4d4d8387528b
ESTHEFANE RAELY DE CASTRO SOUZA <97446792+Esthefane-Raely@users.noreply.github.com>
2023-11-23 18:57:39 -0300

    Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,1 +1,24 @@
-Calculadora.java
+package TrabalhoADS;
+
+public class Calculadora {
+      public int somar(int a, int b) {
+        return a + b;
+    }
+    public void calcularMarkup(Mercadoria mercadoria) {
+        float markup = mercadoria.getMarkup();
+        System.out.println("O markup da mercadoria é: " + markup);
+    }
+    public static void main(String[] args) {
+        Mercadoria produto = new Mercadoria();
+        produto.setCompra(50);
+        produto.setVenda(100);
+      
+        Calculadora calculadora = new Calculadora();
+
+        int resultado = calculadora.somar(10, 20);
+        System.out.println("Resultado da soma: " + resultado);
+      
+        calculadora.calcularMarkup(produto);
+    }
+}
+
